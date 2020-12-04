e7af75962d92e3ccb9bcdee8b28566819ad6e9a2
Alexis <psialedev@gmail.com>
2020-12-04 11:07:02 -0600

    added classes and displayed the first story

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'classes/story_brain.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
@@ -14,6 +15,7 @@ class Destini extends StatelessWidget {
 }
 
 //TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = new StoryBrain();
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -24,7 +26,6 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
         decoration: BoxDecoration(
             image: DecorationImage(
           image: AssetImage("images/universe.jpg"),
@@ -40,9 +41,9 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    "${storyBrain.getStory()}",
                     style: TextStyle(
+                      backgroundColor: Color.fromRGBO(0, 0, 0, .3),
                       fontSize: 25.0,
                     ),
                   ),
